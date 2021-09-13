3be4760204de1bfb233d8befc660ec03ae7b3975
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-09-13 22:45:36 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const AddTab = () => {
+  const tRow = document.createElement('tr');
+  const rScope = document.createElement('th');
+  rScope.scope = 'row';
+  const tCell = document.createElement('td');
+};
