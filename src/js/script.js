f7be5f940afc8174b328de14698a8ca8899a8dd7
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-09-14 01:46:18 +0200

    basic

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,18 @@
-const AddTab = () => {
-  const tRow = document.createElement('tr');
-  const rScope = document.createElement('th');
-  rScope.scope = 'row';
-  const tCell = document.createElement('td');
-};
+function AddTab() {
+  const newMarka = document.getElementById('marka').value;
+  const newModel = document.getElementById('model').value;
+  const newRok = document.getElementById('rok').value;
+  let table = document.getElementById('cars');
+  let x = table.rows.length;
+  let id = 'tbl' + x;
+  let row = table.insertRow(x);
+  row.id = id;
+  let cell1 = row.insertCell(0);
+  let cell2 = row.insertCell(1);
+  let cell3 = row.insertCell(2);
+  let cell4 = row.insertCell(3);
+  cell1.outerHTML = `<th> ${x}</th>`;
+  cell2.innerHTML = newMarka;
+  cell3.innerHTML = newModel;
+  cell4.innerHTML = newRok;
+}
