8922b5a90bb22abb5c71ecf938363107155a66c8
Zbigniew Gawroński <zbigniewgawro@gmail.com>
2021-09-16 11:50:48 +0200

    bit clr and es6

--- src/js/dataTable.js
+++ src/js/dataTable.js
@@ -1,8 +1,35 @@
-const basicData = {
-  columns: ['Marka pojazdu', 'Model pojazdu', 'Rok Produkcji'],
-  rows: [
-    ['Mercedes', 'AMG', '2021'],
-    ['Ford', 'Mustang', '1967'],
-    ['Fiat', '126p', '1985'],
-  ],
+const newTable = (tableData) => {
+  let table = document.getElementById('cars');
+  let tableBody = document.getElementById('tbody');
+  tableData.forEach(function (rowData) {
+    let row = document.createElement('tr');
+    let r = 1;
+    r++;
+    row.setAttribute('id', r);
+
+    rowData.forEach(function (cellData) {
+      let cell = document.createElement('td');
+      cell.setAttribute('contenteditable', 'true');
+      cell.appendChild(document.createTextNode(cellData));
+      row.appendChild(cell);
+      // let cell4 = row.insertCell(3);
+      // cell4.outerHTML = `<th><input type='button' value="Usuń" onClick="DelTab(this)"></th>`;
+    });
+
+    tableBody.appendChild(row);
+  });
 };
+
+newTable([
+  ['Mercedes', 'AMG', '2021'],
+  ['Ford', 'Mustang', '1967'],
+  ['Fiat', '126p', '1985'],
+]);
+
+let tableInfo = Array.prototype.map.call(document.querySelectorAll('table tr'), function (tr) {
+  return Array.prototype.map.call(tr.querySelectorAll('td'), function (td) {
+    return td.innerHTML;
+  });
+});
+
+console.log(tableInfo);
